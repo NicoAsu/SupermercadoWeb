7a32380deca7702d9a1c5eef164c91afe9a78938
Tyrone09876543 <rubik09876543@gmail.com>
2021-08-18 21:59:50 -0400

    Categoria hecha

--- SupermercadoWeb/Models/Categoria.cs
+++ SupermercadoWeb/Models/Categoria.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace SupermercadoWeb.Models
@@ -8,6 +8,7 @@ namespace SupermercadoWeb.Models
     public partial class Categoria
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
         public string Nombre { get; set; }
     }
 }
